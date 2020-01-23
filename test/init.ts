627d901f35276ff3e0f842d0717011455f99b54c
James_Git <franciscusjames01@gmail.com>
2020-01-23 11:06:48 -0300

    environment setup.

---
+++ test/init.ts
@@ -0,0 +1,9 @@
+import * as chai from 'chai';
+//import chaiHttp = require('chai-http');
+
+//chai.use(chaiHttp);
+
+export const assert = chai.assert;
+export const expect = chai.expect;
+//export const request = chai.request(process.env.TEST_URL);
+export const should = chai.should;
