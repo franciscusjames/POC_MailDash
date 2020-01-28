8925724a6ecaf7fd71e736de02d0c044f4a309e6
Mohamad Hicham Tarif <hamudetrf@gmail.com>
2020-01-28 11:01:45 -0300

    update model incident

--- models/incident.js
+++ models/incident.js
@@ -1,14 +1,41 @@
 const mongoose = require('mongoose');
 
 const IncidentSchema = new mongoose.Schema({  
-    Incidente:        String,
-    Autor:            String,
-    DataOcorrencia:   String,
-    Setor:            String,
-    Prioridade:       String,
-    Categoria:        String,        
-    DeptoResponsavel: String,
-    Descricao:        String
+    Incidente:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    Autor:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    DataOcorrencia:{
+        type:Date,
+        default:Date.now,
+        required:true
+    },
+    Setor:{
+        type:String,
+        required:true
+    },
+    Prioridade:{
+    type:String,
+    required:true
+    },
+    Categoria:{
+        type:String,
+        required:true
+    },        
+    DeptoResponsavel:{
+        type:String,
+        required:true
+    },
+    Descricao:{
+        type:String, 
+        required:true
+    }
 }, {
     timestamps: true
 });
