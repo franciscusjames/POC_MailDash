3b4cf503ffcbccd60861da0a551b9a7e5ddb7fc6
Mohamad Hicham Tarif <hamudetrf@gmail.com>
2020-01-29 12:21:58 -0300

    remove console

--- controller/incident.controller.js
+++ controller/incident.controller.js
@@ -1,9 +1,6 @@
 const {Incident} = require("../models");
 
 exports.create = async (mail) => {
-    console.log('remetente')
-    console.log(mail)
     const incident = new Incident(mail)
-    console.log(incident)
     return await incident.save();
 }
