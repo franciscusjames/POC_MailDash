9b0ce4c7147f25be17fcee1bd81bfe7a62cbbbd7
James_Git <franciscusjames01@gmail.com>
2020-02-11 15:09:55 -0300

    delete files.

--- routes/mail.js
+++ routes/mail.js
@@ -5,7 +5,7 @@ const graph = require('@microsoft/microsoft-graph-client');
 require('isomorphic-fetch');
 const htmlToText = require('html-to-text');
 const controller = require('../controller/email.controller');
-const {parseAnexos, deleteAnexosFolders} = require('../utils/parseAnexos')
+const { parseAnexos, deleteAnexosFolders, pdfToText } = require('../utils/parseAnexos')
 const fs = require('fs-extra');
 
 
@@ -13,135 +13,141 @@ let client, emailsTratados;
 let dateTimeParm = '2020-02-03T17:30:00Z';
 
 async function tratarEmails(emails) {
-  emailsTratados = emails.map( (item) => {
-      return {emailId: item.id,
-              tipoEmail: 'Inbox',
-              remetente: item.from.emailAddress.address,
-              assunto: item.subject,
-              emailBody: item.body.content,              
-              dataChegadaOuEnvio: item.receivedDateTime,              
-              attachments: [],
-              hasAttachments: item.hasAttachments,              
-              isRead: item.isRead
-      };
-  });  
+  emailsTratados = emails.map((item) => {
+    return {
+      emailId: item.id,
+      tipoEmail: 'Inbox',
+      remetente: item.from.emailAddress.address,
+      assunto: item.subject,
+      emailBody: item.body.content,
+      dataChegadaOuEnvio: item.receivedDateTime,
+      attachments: [],
+      hasAttachments: item.hasAttachments,
+      isRead: item.isRead
+    };
+  });
 }
 
 async function formatarEmails(emails) {
   let formatList = emails.map((item) => {
-      let textBody = htmlToText.fromString(item.emailBody)
-      //console.log('textBody: ',textBody ) 	  
-	    let clearTextBody = textBody.split('--' || '__')[0];
-      //console.log('clearTextBody: ',clearTextBody ) 
-      return {emailId: item.emailId,
-              tipoEmail: item.tipoEmail,
-              remetente: item.remetente,
-              assunto: item.assunto,
-              emailBody: JSON.parse('{' + clearTextBody + '}'),
-              dataChegadaOuEnvio: item.dataChegadaOuEnvio,              
-              hasAttachments: item.hasAttachments,
-              attachments: item.attachments,                            
-              isRead: item.isRead,
-      };
-  });    
+    let textBody = htmlToText.fromString(item.emailBody)
+    //console.log('textBody: ',textBody ) 	  
+    let clearTextBody = textBody.split('--' || '__')[0];
+    //console.log('clearTextBody: ',clearTextBody ) 
+    return {
+      emailId: item.emailId,
+      tipoEmail: item.tipoEmail,
+      remetente: item.remetente,
+      assunto: item.assunto,
+      emailBody: JSON.parse('{' + clearTextBody + '}'),
+      dataChegadaOuEnvio: item.dataChegadaOuEnvio,
+      hasAttachments: item.hasAttachments,
+      attachments: item.attachments,
+      isRead: item.isRead,
+    };
+  });
   return formatList;
 }
 
 async function getAnexos(emails) {
-  let anexos =  emails.map(async (item) => {   
-      if (item.hasAttachments) {                
-          try {                          
-            const dir = `./Anexos/${item.assunto.replace(':', '')}`;
-            fs.ensureDirSync(dir);
-            
-            const res = await client
-            .api(`/me/messages/${item.emailId}/attachments/`)
-            .get();   
+  let anexos = emails.map(async (item) => {
+    if (item.hasAttachments) {
+      try {
+        const dir = `./Anexos/${item.assunto.replace(':', '')}`;
+        fs.ensureDirSync(dir);
 
-            let attach = res.value;
-            attach.map((anexo) => {
-                fs.writeFileSync(`${dir}/${anexo.name}`, anexo.contentBytes, {encoding: 'base64'})
-                item.attachments.push({fileName:anexo.name });                  
-            });   
-            return item;
+        const res = await client
+          .api(`/me/messages/${item.emailId}/attachments/`)
+          .get();
 
-          } catch (err) {
-            console.log('Erro: ', err);
-          }        
-
-      } else {
+        let attach = res.value;
+        attach.map((anexo) => {
+          fs.writeFileSync(`${dir}/${anexo.name}`, anexo.contentBytes, { encoding: 'base64' })
+          item.attachments.push({ fileName: anexo.name });
+        });
         return item;
+
+      } catch (err) {
+        console.log('Erro: ', err);
       }
-  }); 
-  return Promise.all(anexos);   
+
+    } else {
+      return item;
+    }
+  });
+  return Promise.all(anexos);
 }
 
 
 /* GET /mail */
-router.get('/', async function(req, res, next) {
-    let parms = { title: 'Inbox', active: { inbox: true } };    
-  
-    const accessToken = await authHelper.getAccessToken(req.cookies, res);
-    const userName = req.cookies.graph_user_name;
-  
-    if (accessToken && userName) {
-      parms.user = userName;
-  
-      // Initialize Graph client
-      client = graph.Client.init({
-        authProvider: (done) => {
-          done(null, accessToken);
-        }
-      });           
+router.get('/', async function (req, res, next) {
+  let parms = { title: 'Inbox', active: { inbox: true } };
 
-      //CAIXA DE ENTRADA
-      try {
-        // Get the 10 newest messages from inbox
-        const result = await client
+  const accessToken = await authHelper.getAccessToken(req.cookies, res);
+  const userName = req.cookies.graph_user_name;
+
+  if (accessToken && userName) {
+    parms.user = userName;
+
+    // Initialize Graph client
+    client = graph.Client.init({
+      authProvider: (done) => {
+        done(null, accessToken);
+      }
+    });
+
+    //CAIXA DE ENTRADA
+    try {
+      // Get the 10 newest messages from inbox
+      const result = await client
         .api(`/me/mailfolders/inbox/messages`)
-        .top(1)
+        .top(5)
         //.select('subject,from,receivedDateTime,isRead')
-        .select('*')                                       
+        .select('*')
         .orderby('receivedDateTime DESC')
         //.filter(`receivedDateTime ge ${dateTimeParm}`) //traz a partir dessa data/hora
-        .get();            
-        parms.messages = result.value;            
-        res.render('mail', parms);                        
-        //console.log('result.value: ', result.value);
+        .get();
+      parms.messages = result.value;
+      res.render('mail', parms);
+      //console.log('result.value: ', result.value[4]);
 
-        //FILTRA DADOS DO EMAIL QUE SERAO UTILIZADOS
-        await tratarEmails(result.value);  
-        //console.log('emailsTratados: ', emailsTratados);        
-        
-      } catch (err) {
-        parms.message = 'Error retrieving messages';
-        parms.error = { status: `${err.code}: ${err.message}` };
-        parms.debug = JSON.stringify(err.body, null, 2);
-        res.render('error', parms);
-      }
+      //FILTRA DADOS DO EMAIL QUE SERAO UTILIZADOS
+      await tratarEmails([result.value[4]]);
+      //console.log('emailsTratados: ', emailsTratados);        
 
-      //FORMATA EMAILSBODY (HTML->TEXT->JSON) P/ GRAVAR NO BANCO
-      let emailsFormatados = await formatarEmails(emailsTratados);
-      //console.log('emailsFormatados: ', emailsFormatados);
-
-      //PEGA ANEXOS DOS EMAILS, SE HOUVER
-      let attachedEmailList =  await getAnexos(emailsFormatados); 
-      //console.log('attachedEmailList: ', attachedEmailList);   
-
-      //TRANSFORMA O CONTEUDO DOR ANEXOS EM JSON
-      let finalEmailList = await parseAnexos(attachedEmailList); 
-      console.log('finalEmailList: ', finalEmailList); 
-      
-      //DELETA OS DIRETÓRIOS DOS ANEXOS
-      //await deleteAnexosFolders(finalEmailList);      
-
-      //GRAVA LISTA DE EMAILS NO BANCO      
-      //await controller.save(finalEmailList)      
-
-    } else {
-      // Redirect to home
-      res.redirect('/');
+    } catch (err) {
+      parms.message = 'Error retrieving messages';
+      parms.error = { status: `${err.code}: ${err.message}` };
+      parms.debug = JSON.stringify(err.body, null, 2);
+      res.render('error', parms);
     }
+
+    //FORMATA EMAILSBODY (HTML->TEXT->JSON) P/ GRAVAR NO BANCO
+    let emailsFormatados = await formatarEmails(emailsTratados);
+    //console.log('emailsFormatados: ', emailsFormatados);
+
+    //PEGA ANEXOS DOS EMAILS, SE HOUVER
+    let attachedEmailList = await getAnexos(emailsFormatados);
+    //console.log('attachedEmailList: ', attachedEmailList);   
+
+    //TRANSFORMA O CONTEUDO DOS ANEXOS EM JSON/TXT
+    let parsedEmailList = await parseAnexos(attachedEmailList);
+    //console.log('parsedEmailList: ', parsedEmailList);
+
+    //TRANSFORMA O CONTEUDO TXT(SE HOUVER) EM JSON
+    let finalEmailList = await pdfToText(parsedEmailList);
+    console.log('finalEmailList: ', finalEmailList[0].attachments);
+
+    //DELETA OS DIRETÓRIOS DOS ANEXOS
+    await deleteAnexosFolders(finalEmailList);
+
+    //GRAVA LISTA DE EMAILS NO BANCO      
+    //await controller.save(finalEmailList)      
+
+  } else {
+    // Redirect to home
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
--- utils/parseAnexos.js
+++ utils/parseAnexos.js
@@ -3,82 +3,90 @@ const excelToJson = require('convert-excel-to-json');
 const fs = require('fs-extra');
 
 
-async function parseAnexos(emails) {    
-    let parsedAnexos = await emails.filter(async (item) => {           
-        if (!item.hasAttachments) {          
-			return item;
-		} else {
-            await item.attachments.filter(async (anexo) => {                
-                if (anexo.fileName.includes('.pdf' || '.PDF')) {                  
+async function parseAnexos(emails) {
+    let parsedAnexos = await emails.filter(async (item) => {
+        if (!item.hasAttachments) {
+            return item;
+        } else {
+            await item.attachments.filter(async (anexo) => {
+                if (anexo.fileName.includes('.pdf' || '.PDF')) {
                     let filePath = `./Anexos/${item.assunto.replace(':', '')}/${anexo.fileName}`;
-                    let pdfParser = new PDFParser(this,1);
-                    
-                    pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );	
+                    let pdfParser = new PDFParser(this, 1);
 
-					await pdfParser.on("pdfParser_dataReady", async pdfData => {              					                          
-                        fs.writeFileSync(`./Anexos/${item.assunto.replace(':', '')}/${anexo.fileName.replace('.pdf' || '.PDF', '.txt')}`, 
-                                         pdfParser.getRawTextContent().split('-')[0]);
+                    pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
 
-                        // fs.unlinkSync(filePath, (err) => {
-                        //     if (err) { console.error(`Erro ao deletar anexo '${anexo.fileName}' : `, err); return; }
-                        // });                        
-                    });						                                        
+                    await pdfParser.on("pdfParser_dataReady", async pdfData => {
+                        await fs.writeFileSync(`./Anexos/${item.assunto.replace(':', '')}/${anexo.fileName.replace('.pdf' || '.PDF', '.txt')}`,
+                            pdfParser.getRawTextContent().split('-')[0]);
+                    });
 
                     await pdfParser.loadPDF(filePath);
-                    
-                    anexo.fileContent = JSON.parse('{' + fs.readFileSync(`./Anexos/${item.assunto.replace(':', '')}/${anexo.fileName.replace('.pdf' || '.PDF', '.txt')}`) + '}');
-                    console.log('fileContent: ', anexo);
-                } 
-   
 
-                if (anexo.fileName.includes('.xls' || '.xlsx')) { 
+                    // anexo.fileContent = JSON.parse('{' + fs.readFileSync(`./Anexos/${item.assunto.replace(':', '')}/${anexo.fileName.replace('.pdf' || '.PDF', '.txt')}`) + '}');
+                    // console.log('fileContent: ', anexo);
+                }
+
+
+                if (anexo.fileName.includes('.xls' || '.xlsx')) {
                     let filePath = `./Anexos/${item.assunto.replace(':', '')}/${anexo.fileName}`;
-                    
-					const result = excelToJson({ 
-						sourceFile: filePath
-                    });
-                    
-                    anexo.fileContent = result.Plan1;					
-                    
-                    // fs.unlinkSync(filePath, (err) => {
-                    //     if (err) { console.error(`Erro ao deletar anexo '${anexo.fileName}' : `, err); return; }
-                    // });                    
-                }  
 
-			});	                    
+                    const result = excelToJson({
+                        sourceFile: filePath
+                    });
+
+                    anexo.fileContent = result.Plan1;
+                }
+
+            });
         }
-    });      
-    return Promise.all(parsedAnexos); 
+    });
+    return Promise.all(parsedAnexos);
+}
+
+
+async function pdfToText(emailList) {
+    let finalEmailList = await emailList.filter(async email => {
+        if (email.hasAttachments) {
+
+            await email.attachments.filter(async anexo => {
+                if (anexo.fileName.includes('.pdf' || '.PDF')) {
+                    anexo.fileContent = JSON.parse('{' + fs.readFileSync(`./Anexos/${email.assunto.replace(':', '')}/${anexo.fileName.replace('.pdf' || '.PDF', '.txt')}`) + '}');
+                    //console.log('fileContent: ', anexo);
+                }
+            })
+        }
+    });
+    return Promise.all(finalEmailList);
 }
 
 
 async function deleteAnexosFolders(emailList) {
-    emailList.forEach( async email => {        
+    emailList.forEach(async email => {
         if (email.hasAttachments) {
 
             await email.attachments.forEach(async anexo => {
                 if (anexo.fileName.includes('.pdf' || '.PDF')) {
                     await fs.unlinkSync(`./Anexos/${email.assunto.replace(':', '')}/${anexo.fileName.replace('.pdf' || '.PDF', '.txt')}`, (err) => {
-                        if (err) { console.error(`Erro ao deletar anexo '${anexo.fileName.replace('.pdf' || '.PDF', '.txt')}' : `, err); return ;}                        
-                    });     console.log('DELETOU TXT')
+                        if (err) { console.error(`Erro ao deletar anexo '${anexo.fileName.replace('.pdf' || '.PDF', '.txt')}' : `, err); return; }
+                    }); console.log(`DELETOU '${anexo.fileName.replace('.pdf' || '.PDF', '.txt')}'`)
                     await fs.unlinkSync(`./Anexos/${email.assunto.replace(':', '')}/${anexo.fileName}`, (err) => {
-                        if (err) { console.error(`Erro ao deletar anexo '${anexo.fileName}' : `, err); return; }                    
-                    });     console.log('DELETOU PDF')
+                        if (err) { console.error(`Erro ao deletar anexo '${anexo.fileName}' : `, err); return; }
+                    }); console.log(`DELETOU '${anexo.fileName}'`)
                 } else {
                     await fs.unlinkSync(`./Anexos/${email.assunto.replace(':', '')}/${anexo.fileName}`, (err) => {
-                        if (err) { console.error(`Erro ao deletar anexo '${anexo.fileName}' : `, err); return; }                    
-                    });     console.log('DELETOU XLS')
+                        if (err) { console.error(`Erro ao deletar anexo '${anexo.fileName}' : `, err); return; }
+                    }); console.log(`DELETOU '${anexo.fileName}'`)
                 }
             });
-            
+
 
             await fs.rmdir(`./Anexos/${email.assunto.replace(':', '')}`, (err) => {
-                console.log('DELETANDO FOLDER')
+                console.log(`DELETANDO FOLDER '${email.assunto.replace(':', '')}'`)
                 if (err) { console.error('Erro ao deletar diretório de anexos: ', err); return; }
             });
-        }                
+        }
     });
 }
 
 
-module.exports = {parseAnexos, deleteAnexosFolders}
+module.exports = { parseAnexos, deleteAnexosFolders, pdfToText }
